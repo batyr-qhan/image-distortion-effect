3fba2e3b2f1410b5ea69523f27b2720410ae7357
Batyrkhan Zhaparov <54882375+batyr-qhan@users.noreply.github.com>
2025-01-06 15:48:14 +0500

    refactor styles and structure for image parts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import "./App.css";
 
 function App() {
-    const numParts = 30; // Number of parts to divide the image into
-    const parts = Array.from({ length: numParts }, (_, i) => <div key={i} className="part"></div>);
-    const parts2 = Array.from({ length: numParts }, (_, i) => <div key={i} className="part2"></div>);
+  const numParts = 30;
+  const parts = Array.from({ length: numParts }, (_, i) => (
+    <div key={i} className="part"></div>
+  ));
+  const parts2 = Array.from({ length: numParts }, (_, i) => (
+    <div key={i} className="part2"></div>
+  ));
 
-    return (
-        <div className="App">
-            <div className="images-wrapper">
-                <div className="image">{parts}</div>
-                <div className="image">{parts2}</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <div className="images-wrapper">
+        <div className="image">{parts}</div>
+        <div className="image">{parts2}</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+function generatePositionStyles() {
+  const style = document.createElement('style');
+  let css = '';
+  
+  for (let i = 1; i <= 30; i++) {
+    const position = ((i - 1) * 3.33).toFixed(2);
+    css += `
+      .part:nth-child(${i}),
+      .part2:nth-child(${i}),
+      .image:hover .part:nth-child(${i}),
+      .image:hover .part2:nth-child(${i}) {
+        background-position: ${position}% 0;
+      }
+    `;
+  }
+  
+  style.textContent = css;
+  document.head.appendChild(style);
+}
+
+generatePositionStyles();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
